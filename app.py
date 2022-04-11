2eeae73e4c1ce7063d273548617358e8c155f32d
waterquarks <waterquarks@gmail.com>
2022-04-11 15:45:05 -0500

    Calculate FTX slippages in memory (#6)

--- app.py
+++ app.py
@@ -516,7 +516,79 @@ def analytics_ftx_slippages():
 
     db.row_factory = sqlite3.Row
 
-    entries = list(map(dict, db.execute("select datetime(max(timestamp), 'unixepoch') as timestamp, market, order_size, buy_slippage_bps, sell_slippage_bps, total_slippage_bps from slippages group by market, order_size")))
+    state = sqlite3.connect(':memory:')
+
+    state.row_factory = sqlite3.Row
+
+    state.execute("create temp table slippages (timestamp text, market text, order_size real, buy_slippage_bps real, sell_slippage_bps real, total_slippage_bps real, primary key (timestamp, market, order_size)) without rowid")
+
+    state.execute("attach database './scrapers/ftx/l2_order_book_analytics.db' as analytics")
+
+    query = """
+        insert or replace into slippages
+        with
+            orders as (
+                select
+                    side,
+                    price,
+                    sum(size) as size,
+                    price * sum(size) as volume,
+                    sum(price * sum(size)) over (partition by side order by case when side = 'bids' then - price when side = 'asks' then price end) as cumulative_volume
+                from analytics.orders
+                where market = :market
+                group by side, price
+                order by side, case when side = 'bids' then - price when side = 'asks' then price end
+            ),
+            misc as (
+                select
+                    *,
+                    (top_bid + top_ask) / 2 as mid_price
+                from (
+                    select
+                        max(price) filter ( where side = 'bids') as top_bid,
+                        min(price) filter ( where side = 'asks') as top_ask
+                    from orders
+                )
+            ),
+            fills as (
+                select
+                    side, price, fill, sum(fill) over (partition by side order by case when side = 'bids' then - price when side = 'asks' then price end) as cumulative_fill
+                from (
+                    select
+                        side,
+                        price,
+                        case when cumulative_volume < :order_size then volume else coalesce(lag(remainder) over (partition by side), case when volume < :order_size then volume else :order_size end) end as fill
+                    from (select *, :order_size - cumulative_volume as remainder from orders)
+                )
+                where fill > 0
+            ),
+
+            weighted_average_fill_prices as (
+                select
+                    case when sum(case when side = 'asks' then fill end) = :order_size then sum(case when side = 'asks' then price * fill end) / :order_size end as weighted_average_buy_price,
+                    case when sum(case when side = 'bids' then fill end) = :order_size then sum(case when side = 'bids' then price * fill end) / :order_size end as weighted_average_sell_price
+                from fills
+            )
+        select
+            *,
+            buy_slippage_bps + sell_slippage_bps as total_slippage_bps
+        from
+        (
+            select
+                :timestamp as timestamp,
+                :market as market,
+                :order_size as order_size,
+                ((weighted_average_buy_price - mid_price) / mid_price) * 1e4 as buy_slippage_bps,
+                ((mid_price - weighted_average_sell_price) / mid_price) * 1e4 as sell_slippage_bps
+            from weighted_average_fill_prices, misc
+        )
+    """
+
+    for market in perpetuals:
+        for order_size in [50000, 100000, 200000, 500000, 1000000]:
+            state.execute(query, {'timestamp': 20000, 'market': market, 'order_size': order_size})
+
+    entries = list(map(dict, state.execute("select datetime(max(timestamp), 'unixepoch') as timestamp, market, order_size, buy_slippage_bps, sell_slippage_bps, total_slippage_bps from slippages group by market, order_size")))
 
     return render_template(
         'ftx_slippages.html',
