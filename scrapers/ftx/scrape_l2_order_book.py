cff2911166310ce4d6c6807fef8c535172fcac2c
waterquarks <waterquarks@gmail.com>
2022-04-11 15:43:29 -0500

    Calculate slippage in-memory

--- scrapers/ftx/scrape_l2_order_book.py
+++ scrapers/ftx/scrape_l2_order_book.py
@@ -61,10 +61,6 @@ async def main():
 
     db.execute("create table if not exists orders (market text, side text, price real, size real, primary key (market, side, price)) without rowid")
 
-    db.execute("create table if not exists liquidity (timestamp text, market text, buy real, sell real, primary key (timestamp, market)) without rowid")
-
-    db.execute("create table if not exists slippages (timestamp text, market text, order_size real, buy_slippage_bps real, sell_slippage_bps real, total_slippage_bps real, primary key (timestamp, market, order_size)) without rowid")
-
     async for entry in ingestor():
         if entry['type'] not in { 'partial', 'update' }:
             continue
@@ -81,82 +77,7 @@ async def main():
                 else:
                     db.execute('insert or replace into orders values (?, ?, ?, ?)', [entry['market'], side, price, size])
         else:
-            db.execute("""
-                insert or replace into liquidity
-                select
-                    ? as timestamp,
-                    market,
-                    sum(case when market = ? and side = 'bids' then price * size end) as buy,
-                    sum(case when market = ? and side = 'asks' then price * size end) as sell
-                from orders
-            """, [entry['data']['time'], entry['market'], entry['market']])
-
-            query = """
-                insert or replace into slippages
-                with
-                    orders as (
-                        select
-                            side,
-                            price,
-                            sum(size) as size,
-                            price * sum(size) as volume,
-                            sum(price * sum(size)) over (partition by side order by case when side = 'bids' then - price when side = 'asks' then price end) as cumulative_volume
-                        from main.orders
-                        where market = :market
-                        group by side, price
-                        order by side, case when side = 'bids' then - price when side = 'asks' then price end
-                    ),
-                    misc as (
-                        select
-                            *,
-                            (top_bid + top_ask) / 2 as mid_price
-                        from (
-                            select
-                                max(price) filter ( where side = 'bids') as top_bid,
-                                min(price) filter ( where side = 'asks') as top_ask
-                            from orders
-                        )
-                    ),
-                    fills as (
-                        select
-                            side, price, fill, sum(fill) over (partition by side order by case when side = 'bids' then - price when side = 'asks' then price end) as cumulative_fill
-                        from (
-                            select
-                                side,
-                                price,
-                                case when cumulative_volume < :order_size then volume else coalesce(lag(remainder) over (partition by side), case when volume < :order_size then volume else :order_size end) end as fill
-                            from (select *, :order_size - cumulative_volume as remainder from orders)
-                        )
-                        where fill > 0
-                    ),
-                
-                    weighted_average_fill_prices as (
-                        select
-                            case when sum(case when side = 'asks' then fill end) = :order_size then sum(case when side = 'asks' then price * fill end) / :order_size end as weighted_average_buy_price,
-                            case when sum(case when side = 'bids' then fill end) = :order_size then sum(case when side = 'bids' then price * fill end) / :order_size end as weighted_average_sell_price
-                        from fills
-                    )
-                select
-                    *,
-                    buy_slippage_bps + sell_slippage_bps as total_slippage_bps
-                from
-                (
-                    select
-                        :timestamp as timestamp,
-                        :market as market,
-                        :order_size as order_size,
-                        ((weighted_average_buy_price - mid_price) / mid_price) * 1e4 as buy_slippage_bps,
-                        ((mid_price - weighted_average_sell_price) / mid_price) * 1e4 as sell_slippage_bps
-                    from weighted_average_fill_prices, misc
-                )
-            """
-
-            for order_size in [50000, 100000, 200000, 500000, 1000000]:
-                db.execute(query, {'timestamp': entry['data']['time'], 'market': entry['market'], 'order_size': order_size})
-
             db.commit()
-
-
 
 
 if __name__ == '__main__':
